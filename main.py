c000200f5726809a970fdfa32c65350c5a695d44
MAHAN <mahan78ir@gmail.com>
2025-08-05 21:44:13 +0330

    get all items done

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-
+from fastapi.responses import JSONResponse
 
 
 @asynccontextmanager
@@ -10,4 +10,19 @@ async def lifespan(app:FastAPI):
     print("Application shutdown")
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+
+custom_list = [
+    {"id" : 1 , "amount" : 100 , "description" : "for sport"},
+    {"id" : 2 , "amount" : 150 , "description" : "for lunch"},
+    
+]
+
+
+
+
+@app.get("/items/")
+async def read_items():
+    return JSONResponse(content=custom_list)
+
+
