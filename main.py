477b8edc2b268c3e30ec582ac71e6311438a0dcd
MAHAN <mahan78ir@gmail.com>
2025-08-05 21:27:32 +0330

    Create App

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print("Application startup")
+    yield
+    print("Application shutdown")
+
+
+app = FastAPI()
